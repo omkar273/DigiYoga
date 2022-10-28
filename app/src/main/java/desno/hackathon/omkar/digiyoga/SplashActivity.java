2bafca0817b9a78ce437e2765c171ecbe1581a9d
omkar sonawane <omkarsonawane62@gmail.com>
2022-10-28 20:14:06 +0530

    initial commit
    Signed-off-by: omkar sonawane <omkarsonawane62@gmail.com>

---
+++ app/src/main/java/desno/hackathon/omkar/digiyoga/SplashActivity.java
@@ -0,0 +1,41 @@
+package desno.hackathon.omkar.digiyoga;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+    TextView app_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        logo = findViewById(R.id.splash_screen_logo);
+        app_name = findViewById(R.id.app_name);
+
+        Animation zoomOut = AnimationUtils.loadAnimation(this, R.anim.splash_logo_animation);
+        logo.startAnimation(zoomOut);
+        app_name.startAnimation(zoomOut);
+
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
---
+++ app/src/main/java/desno/hackathon/omkar/digiyoga/StartJourneyActivity.java
@@ -0,0 +1,14 @@
+package desno.hackathon.omkar.digiyoga;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartJourneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_journey);
+    }
+}
