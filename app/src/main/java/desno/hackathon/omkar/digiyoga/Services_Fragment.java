ef4454953581086a9d240d0bce8e89107329700e
omkar sonawane <omkarsonawane62@gmail.com>
2022-11-18 23:06:35 +0530

    adding live meet
    Signed-off-by: omkar sonawane <omkarsonawane62@gmail.com>

---
+++ app/src/main/java/desno/hackathon/omkar/digiyoga/Services_Fragment.java
@@ -0,0 +1,27 @@
+package desno.hackathon.omkar.digiyoga;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Services_Fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_services_, container, false);
+
+
+        return view;
+    }
+}
